f1c4445e97cd48082e80dbfdb6d20d2b4f035963
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-05-09 18:40:58 +0100

    Doc serve

--- src/api-notas/server.ts
+++ src/api-notas/server.ts
@@ -1,9 +1,17 @@
-
 import * as net from 'net';
 import { RequestType, ResponseType } from './types';
 import { addTodo} from './commands';
 import { MessageEventEmitterClient } from '../ejercicio-pe102/eventEmitterClient';
 
+/**
+ * Función todoFields, maneja las peticiones y si se introducen correctamente los tipos de atributos
+ * para el manejo de nota, comprobando que sean de tipo string
+ * @param request petición del servidor 
+ * @param titleIn titulo entrante a revisar de la nota
+ * @param bodyIn cuerpo entrante a revisar de la nota
+ * @param colorIn color entrante a revisar de la nota
+ * @returns true si se cumple que todos los anteriores atributos de la petición son tipo string
+ */
 function todoFields(request: RequestType, titleIn: boolean, bodyIn: boolean, colorIn: boolean): boolean {
   if (titleIn && typeof request.title !== 'string') {
     return false;
@@ -17,6 +25,14 @@ function todoFields(request: RequestType, titleIn: boolean, bodyIn: boolean, col
   return true;
 }
 
+/**
+ * Función processRequest procesa los tipos de peticiones, por ahora intentadas add
+ * añadir notas
+ * @param request petición al servidor
+ * @returns respuesta al añadido de la nota por petición en caso contrario se impondrá que no fue
+ * satisfactorio con success: false, un atributo de la respuesta de servidor que indica a modo de
+ * localizador de errores
+ */
 function processRequest(request: RequestType): ResponseType {
   if (typeof request.type === 'string' && typeof request.user === 'string') {
     if (request.type == 'add') {
@@ -33,6 +49,12 @@ function processRequest(request: RequestType): ResponseType {
   }
 }
 
+/**
+ * En la siguiente parte se crea un servidor que está escuchando
+ * y escribiendo todas las respuesta con respecto a la peticiones que
+ * se realizan para ello se escribirán en notas.json con las que se 
+ * trabajará
+ */
 const server = net.createServer((connection) => {
   const listener = new MessageEventEmitterClient(connection);
   listener.on('request', (request: RequestType) => {
